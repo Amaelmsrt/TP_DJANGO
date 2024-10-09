1aa77bbbb2f3d7efe3c6fa5ef3a423ffcd24d669
Maserati Amael <amael.maserati@etu.univ-orleans.fr>
2024-10-09 11:55:48 +0200

    ajout de classes dans le model

--- monprojet/monapp/models.py
+++ monprojet/monapp/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from django.utils import timezone
 
@@ -8,10 +7,6 @@ PRODUCT_STATUS = (
     (2, 'Out of stock')              
 )
 
-# Create your models here.
-"""
-    Status : numero, libelle
-"""
 class Status(models.Model):
     numero  = models.IntegerField()
     libelle = models.CharField(max_length=100)
@@ -19,11 +14,7 @@ class Status(models.Model):
     def __str__(self):
         return "{0} {1}".format(self.numero, self.libelle)
     
-"""
-Produit : nom, code, etc.
-"""
 class Product(models.Model):
-
     class Meta:
         verbose_name = "Produit"
 
@@ -32,16 +23,44 @@ class Product(models.Model):
     price_ht      = models.DecimalField(max_digits=8, decimal_places=2,  null=True, blank=True, verbose_name="Prix unitaire HT")
     price_ttc     = models.DecimalField(max_digits=8, decimal_places=2,  null=True, blank=True, verbose_name="Prix unitaire TTC")
     status        = models.SmallIntegerField(choices=PRODUCT_STATUS, default=0)
-    date_creation =  models.DateTimeField(blank=True, verbose_name="Date création", default=timezone.now)
-    
+    date_creation = models.DateTimeField(blank=True, verbose_name="Date création", default=timezone.now)
+    suppliers  = models.ManyToManyField("Supplier", related_name="products", through='ProductSupplier')
+
     def __str__(self):
         return "{0} {1}".format(self.name, self.code)
 
-"""
-    Déclinaison de produit déterminée par des attributs comme la couleur, etc.
-"""
+class ProductSupplier(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    supplier = models.ForeignKey('Supplier', on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2,  null=True, blank=True, verbose_name="Prix unitaire HT")
+
+    class Meta:
+        unique_together = ('product', 'supplier')
+
+class Order(models.Model):
+    STATUS = (
+        (0, 'En préparation'),
+        (1, 'Passée'),
+        (2, 'Reçue'),
+    )
+
+    supplier = models.ForeignKey('Supplier', on_delete=models.CASCADE)
+    status = models.SmallIntegerField(choices=STATUS, default=0)
+    date_creation = models.DateTimeField(blank=True, verbose_name="Date création", default=timezone.now)
+    date_update = models.DateTimeField(blank=True, verbose_name="Date modification", default=timezone.now)
+
+    def __str__(self):
+        return "{0} {1}".format(self.supplier, self.date_creation)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey('Order', related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "{0} ({1})".format(self.product.name, self.quantity)
+
 class ProductItem(models.Model):
-    
     class Meta:
         verbose_name = "Déclinaison Produit"
 
@@ -53,23 +72,15 @@ class ProductItem(models.Model):
         return "{0} {1}".format(self.product, self.code)
     
 class ProductAttribute(models.Model):
-    """
-    Attributs produit
-    """
-    
     class Meta:
         verbose_name = "Attribut"
         
-    name =  models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     
     def __str__(self):
         return self.name
     
 class ProductAttributeValue(models.Model):
-    """
-    Valeurs des attributs
-    """
-    
     class Meta:
         verbose_name = "Valeur attribut"
         ordering = ['position']
@@ -81,3 +92,14 @@ class ProductAttributeValue(models.Model):
     def __str__(self):
         return "{0} [{1}]".format(self.value, self.product_attribute)
 
+class Supplier(models.Model):
+    class Meta:
+        verbose_name = "Supplier"
+    
+    name = models.CharField(max_length=100)
+    address = models.TextField(null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
